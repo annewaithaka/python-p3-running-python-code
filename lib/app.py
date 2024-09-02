7c76a80761183a5ef29b4dbfeaa3c1a1994b43f6
annewaithaka <annewaithaka68@gmail.com>
2024-09-02 13:14:11 +0300

    done

--- lib/app.py
+++ lib/app.py
@@ -1,3 +1,4 @@
-print("Hello world!", end=" ")
-print("Hello sun!", end="!! ")
-print("Hello sky!", end="!!!\n")
+# print("Hello world!", end=" ")
+# print("Hello sun!", end="!! ")
+# print("Hello sky!", end="!!!\n")
+print("Hello World! Pass this test, please.")
